35489a28dc27cafbf76fd10a8935076cf8a6b5a7
Cleop <cleo.pearson@hotmail.co.uk>
2018-01-23 12:22:37 +0000

    #7 #8 Initial blog setup

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var path = require('path');
 var apos = require('apostrophe')({
   shortName: 'quodl-site',
 
+  bundlers: ['apostrophe-blog'],
   modules: {
+    'apostrophe-blog': {},
+    'apostrophe-blog-pages': {},
+    'apostrophe-blog-widgets': {},
+    'as-helpers': {},
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
   }
 });
